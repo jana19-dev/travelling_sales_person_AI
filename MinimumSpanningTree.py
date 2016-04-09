8f12ce8c42183b2756f2a2d3ee82a203be633068
Jana <jana.stc@gmail.com>
2016-04-08 21:23:53 -0400

    modified MST to suit our state space

--- MinimumSpanningTree.py
+++ MinimumSpanningTree.py
@@ -27,6 +27,33 @@ def MinimumSpanningTree(G):
     # implement once UnionFind exists, and second, because the only slow
     # part (the sort) is sped up by being built in to Python.
     subtrees = UnionFind()
+    tree = []
+    for W,u,v in sorted((G[u][v],u,v) for u in G for v in G[u]):
+        if subtrees[u] != subtrees[v]:
+            tree.append((u,v))
+            subtrees.union(u,v)
+    return tree
+
+def MinimumSpanningCost(G):
+    """
+    Return the minimum spanning tree of an undirected graph G.
+    G should be represented in such a way that iter(G) lists its
+    vertices, iter(G[u]) lists the neighbors of u, G[u][v] gives the
+    length of edge u,v, and G[u][v] should always equal G[v][u].
+    The tree is returned as a list of edges.
+    """
+    if not isUndirected(G):
+        raise ValueError("MinimumSpanningTree: input is not undirected")
+    for u in G:
+        for v in G[u]:
+            if G[u][v] != G[v][u]:
+                raise ValueError("MinimumSpanningTree: asymmetric weights")
+
+    # Kruskal's algorithm: sort edges by weight, and add them one at a time.
+    # We use Kruskal's algorithm, first because it is very simple to
+    # implement once UnionFind exists, and second, because the only slow
+    # part (the sort) is sped up by being built in to Python.
+    subtrees = UnionFind()
     #tree = []
     cost = 0
     for W,u,v in sorted((G[u][v],u,v) for u in G for v in G[u]):
